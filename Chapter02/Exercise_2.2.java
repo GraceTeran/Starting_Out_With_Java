08ddb4c3e82bf9287aa0344d997b03e464d37201
GraceTeran <80122599+GraceTeran@users.noreply.github.com>
2021-03-20 11:17:36 -0500

    Create Exercise_2.2.java

---
+++ Chapter02/Exercise_2.2.java
@@ -0,0 +1,22 @@
+class Main {
+  public static void main(String[] args) {
+    
+   String firstName, middleName, lastName;
+
+   firstName = "Gene";
+   middleName = "Thomas"; 
+   lastName = "Belcher";
+
+   char firstInitial, middleInitial, lastInitial;
+
+   firstInitial = 'G';
+   middleInitial = 'T';
+   lastInitial = 'B';
+
+   System.out.println(firstName + " " + middleName + " " + lastName);
+   System.out.println(firstInitial + "." + middleInitial + "." + lastInitial + ".");
+
+
+ 
+  }
+}
