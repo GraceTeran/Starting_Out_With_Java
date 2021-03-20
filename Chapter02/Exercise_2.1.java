bcca2547f996478d9520cc12fc34d2cc6075c5d1
GraceTeran <80122599+GraceTeran@users.noreply.github.com>
2021-03-20 11:03:52 -0500

    Create Exercise_2.1.java

---
+++ Chapter02/Exercise_2.1.java
@@ -0,0 +1,17 @@
+class Main {
+  public static void main(String[] args) {
+    String name; 
+    int age = 0;
+    double annualPay = 0;
+
+    name = "Joe Mahoney";
+    age = 26;
+    annualPay = 100000.0;
+    
+
+    System.out.print("My name is " + name + ", ");
+    System.out.print("my age is " + age + " and I hope to earn ");
+    System.out.print( annualPay + " per year. "); 
+    
+  }
+}
